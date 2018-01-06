ae68680610f4767d585e4ad17797339e6af44eee
Lanthu3 <dmatttqd@gmail.com>
2018-01-06 22:19:03 +0700

    'push'

--- countdown/settings.py
+++ countdown/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')9zmb_5d=7+av_(gvn_d-25tw+&ovpfyg=cey%rp0zt@9%nd$%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
